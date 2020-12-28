798e0bb70d91149a70983285b70d29d03d346e0d
ccastro01 <alvescastro00cca13@gmail.com>
2020-12-27 22:03:03 -0300

    Testes com useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(()=> {
+    console.log('did mount')
+  },[])
+
+  useEffect(()=> {
+    console.log('did update:', count)
+  },[count])
 
   return (
     <div className="App">
       <h1>{count}</h1>
 
-      <button
+      {/* <button
         onClick={() =>
           setCount((oldState) => {
             return oldState + 1;
           })
+        }
+      >
+        Increment
+      </button> */}
+
+      <button
+        onClick={() =>
+          setCount(count + 1)
         }
       >
         Increment

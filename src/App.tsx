c6b22abd56907e584165ae56802bb54651113f32
ccastro01 <alvescastro00cca13@gmail.com>
2021-01-11 21:05:56 -0300

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import { useState, useMemo } from "react";
-import Teste from './components/Teste'
+import { useState, useRef, useCallback } from "react";
+import Teste from "./components/Teste";
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [count, setCount] = useState(0);
 
-  const testComponent = useMemo(() => <Teste/>, []);
+  const handleClick = useCallback(() => {
+    if (inputRef.current) {
+      inputRef.current.value = "Chris";
+    }
+  }, []);
 
   return (
     <div className="App">
-
-      {testComponent}
       <h1>{count}</h1>
 
+      <input ref={inputRef} />
 
-      <button
-        onClick={() =>
-          setCount(count + 1)
-        }
-      >
-        Increment
-      </button>
+      <button onClick={handleClick}>Increment</button>
     </div>
   );
 }
